255eb1fa0a18be4573d0988938a17e3ce52eac11
felixrp74 <felixrp74@gmail.com>
2022-10-22 22:31:59 -0500

    location backgraound and notification

---
+++ app/src/main/java/com/example/locationbackground/LocationClient.kt
@@ -0,0 +1,12 @@
+package com.example.locationbackground
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+
+    fun getLocationUpdates(interval: Long): Flow<Location>
+
+    class LocationException(message: String): Exception()
+
+}
--- app/src/main/java/com/example/locationbackground/MainActivity.kt
+++ app/src/main/java/com/example/locationbackground/MainActivity.kt
@@ -1,43 +1,62 @@
 package com.example.locationbackground
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.locationbackground.ui.theme.LocationBackgroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+
+            ),
+            0
+        )
+
         setContent {
             LocationBackgroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Button(onClick = {
+                        Intent(applicationContext,LocationService::class.java).apply {
+                            action = LocationService.ACTION_START
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "start")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        Intent(applicationContext,LocationService::class.java).apply {
+                            action = LocationService.ACTION_STOP
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "stop")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LocationBackgroundTheme {
-        Greeting("Android")
     }
 }
